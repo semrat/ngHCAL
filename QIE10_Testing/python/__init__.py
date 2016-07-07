92d9d670af66b8e59f68dcd5fb870a9cf4d62df8
jmariano <jmariano@terpmail.umd.edu>
2016-07-07 16:20:06 +0200

    cleared python directory

--- QIE10_Testing/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/ngHCAL/QIE10_Testing/',1)[0])+'/cfipython/slc6_amd64_gcc493/ngHCAL/QIE10_Testing')
